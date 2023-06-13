ae0aa28c2f48293c9af8947cb2bb316f6b51f7cc
hellun205 <hellun515@gmail.com>
2023-06-13 10:05:43 +0900

    player movement and animation

---
+++ Assets/Scripts/Isometric/PlayerMovement.cs
@@ -0,0 +1,33 @@
+using System;
+using UnityEngine;
+
+namespace Isometric
+{
+  public class PlayerMovement : MonoBehaviour
+  {
+    [SerializeField]
+    private new Rigidbody2D rigidbody;
+
+    [SerializeField]
+    private PlayerAnimation playerAnimation;
+    
+    private Vector2 moveVector = new Vector2();
+    
+    public float moveSpeed = 4f;
+
+    private void Update()
+    {
+      moveVector.x = Input.GetAxisRaw("Horizontal");
+      moveVector.y = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+      var curPos = rigidbody.position;
+      var inputVec = moveVector.normalized * (moveSpeed * Time.fixedDeltaTime);
+      rigidbody.MovePosition(curPos + inputVec);
+      
+      playerAnimation.SetDirection(moveVector);
+    }
+  }
+}
